a95c112830eb2fc9a46a08fd24e49decfd935530
onsystem <onsystem@proton.me>
2023-11-05 10:40:32 +0100

    changes: create docker file, database, and jpa repostiories

--- code/wscapp-pantheon-api/wscapp-pantheon-api-interface/src/test/java/com/onsystem/wscapp/pantheon/api/interfaces/repositories/ApplicationRepositoryUnitTest.java
+++ code/wscapp-pantheon-api/wscapp-pantheon-api-interface/src/test/java/com/onsystem/wscapp/pantheon/api/interfaces/repositories/ApplicationRepositoryUnitTest.java
@@ -5,24 +5,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class ApplicationRepositoryUnitTest {
+class ApplicationRepositoryUnitTest {
 
     @Autowired
     private ApplicationRepository applicationRepository;
@@ -30,7 +25,7 @@ public class ApplicationRepositoryUnitTest {
 
     @Test
     @Rollback
-    void findById()  {
+    void findById() {
         final ApplicationEntity applicationEntity = applicationRepository.save(
                 ApplicationEntity.builder()
                         .description("asd")
