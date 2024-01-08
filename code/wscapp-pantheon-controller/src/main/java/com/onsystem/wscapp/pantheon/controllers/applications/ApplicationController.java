7d75e4d86f537e479bed3d3bd8752b8800b71429
OnSystem <onsystem@proton.me>
2024-01-08 23:13:36 +0100

    changes: controller application validation and request mapping

--- code/wscapp-pantheon-controller/src/main/java/com/onsystem/wscapp/pantheon/controllers/applications/ApplicationController.java
+++ code/wscapp-pantheon-controller/src/main/java/com/onsystem/wscapp/pantheon/controllers/applications/ApplicationController.java
@@ -4,8 +4,11 @@ import com.onsystem.wscapp.pantheon.GenericView;
 import com.onsystem.wscapp.pantheon.api.dto.applications.application.*;
 import com.onsystem.wscapp.pantheon.api.interfaces.services.applications.create.ICreateApplicationService;
 import com.onsystem.wscapp.pantheon.api.interfaces.services.applications.update.IUpdateApplicationService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,7 +17,9 @@ import java.util.Set;
 import static com.onsystem.wscapp.pantheon.api.interfaces.Constants.ENDPOINT_APPLICATION_ROOT_CONTROLLER;
 import static com.onsystem.wscapp.pantheon.api.interfaces.Constants.ENDPOINT_TO_LANGUAGE;
 
-@RestController(ENDPOINT_APPLICATION_ROOT_CONTROLLER)
+@RestController
+@RequestMapping(ENDPOINT_APPLICATION_ROOT_CONTROLLER)
+@Validated
 public class ApplicationController {
 
     @Autowired
@@ -24,7 +29,7 @@ public class ApplicationController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public GenericView<ApplicationDTO> createApplication(@RequestBody CreateApplicationDTO createApplication) {
+    public GenericView<ApplicationDTO> createApplication(@RequestBody @Valid CreateApplicationDTO createApplication) {
         return GenericView.<ApplicationDTO>builder()
                 .response(iCreateApplicationService.createApplication(createApplication))
                 .build();
@@ -32,29 +37,25 @@ public class ApplicationController {
 
     @PostMapping(ENDPOINT_TO_LANGUAGE)
     @ResponseStatus(HttpStatus.OK)
-    public GenericView<?> createLanguageApplication(
-            @RequestParam Integer applicationId,
-            @RequestBody Set<CreateApplicationLanguageDTO> createApplicationLanguage) {
+    public GenericView<Set<ApplicationLanguageDTO>> createLanguageApplication(
+            @RequestParam @Positive Integer applicationId,
+            @RequestBody @Valid Set<CreateApplicationLanguageDTO> createApplicationLanguage) {
 
-        return GenericView.builder()
+        return GenericView.<Set<ApplicationLanguageDTO>>builder()
                 .response(iCreateApplicationService.createApplicationLanguages(applicationId, createApplicationLanguage))
                 .build();
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public GenericView<?> updateApplication(@RequestBody UpdateApplicationDTO updateApplicationDTO) {
+    public void updateApplication(@RequestBody @Valid UpdateApplicationDTO updateApplicationDTO) {
         iUpdateApplicationService.updateApplication(List.of(updateApplicationDTO));
-        return GenericView.builder()
-                .build();
     }
 
     @PutMapping(ENDPOINT_TO_LANGUAGE)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public GenericView<?> updateApplicationLanguage(@RequestBody Set<UpdateApplicationLanguageDTO> updateApplicationLanguageDTO) {
+    public void updateApplicationLanguage(@RequestBody @Valid Set<UpdateApplicationLanguageDTO> updateApplicationLanguageDTO) {
         iUpdateApplicationService.updateApplicationLanguages(updateApplicationLanguageDTO);
-        return GenericView.builder()
-                .build();
     }
 
 
